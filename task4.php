6e358a1913ae884d5187f165f42746281494f604
Aneela-Codes <63118445+Aneela-Codes@users.noreply.github.com>
2021-10-30 09:07:22 -0700

    customzing cells

--- task4.php
+++ task4.php
@@ -6,7 +6,7 @@
 <body>
 <h1>TASK-4</h1>
 <div>
-<table border="3" cellpadding = "3" cellspacing = "0">
+<table border="3" cellpadding = "3" cellspacing = "2">
 <?php
 for($i=1; $i<=6; $i++){
 echo "<tr>";
