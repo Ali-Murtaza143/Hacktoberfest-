3639225b02919036245a06835009f7118e2736a4
Hina Lilaram <hinalilaram@gmail.com>
2021-10-30 19:18:22 +0500

    added default loop value

--- task1.php
+++ task1.php
@@ -5,13 +5,13 @@
 </head>
 <body>
 <h1>TASK-1</h1>
-<form method="POST" action="" >
+<form method="POST">
 <input type="text" name="number" placeholder="Enter a number">
 <input type="submit" value="submit" name="submit">
 </form>
 <?php
-if(isset($_POST["submit"])){
-$number = $_POST["number"];
+if($_POST["submit"]){
+$number = isset($_POST["number"]) ? $_POST['number'] : "0" ; // default value = zero
 for($i=1; $i<=10; $i++){
 $ans = $i*$number;
 echo "<p>" .$number . " x " . $i . " = ". $ans. "</p>";
